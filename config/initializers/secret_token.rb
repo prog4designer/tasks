624bf9ee9f03d82b6eed6265c8d949f3f723491a
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-05-21 01:41:47 +0900

    r new tasks --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '2531cfe13b6e77d5a04356a5cf87d82e385587efbfc357dfaaca8f1849ba56566cf81a87558e9d1409bf85ca010b23b3326b17544c83e3278e9b484dec756c4c'
