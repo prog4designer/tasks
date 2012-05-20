c83ac47edadc8bc4a2f8c2aa663667287308dd25
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-05-21 01:56:55 +0900

    first version

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -44,7 +44,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to todos_path, notice: 'Todo was successfully created.' }
         format.json { render json: @todo, status: :created, location: @todo }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.update_attributes(params[:todo])
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to todos_path, notice: 'Todo was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
